04ed341b2a5477340a4ea127a07616942c2c2e30
Ruddarm <ruddarmmaurya@gmail.com>
2025-08-03 12:49:31 +0530

    initiate authguared app service

---
+++ authguard-app-service/src/main/java/com/authguard/authguard_app_service/AuthguardAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.authguard.authguard_app_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthguardAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthguardAppServiceApplication.class, args);
+	}
+
+}
---
+++ authguard-app-service/src/test/java/com/authguard/authguard_app_service/AuthguardAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authguard.authguard_app_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthguardAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
